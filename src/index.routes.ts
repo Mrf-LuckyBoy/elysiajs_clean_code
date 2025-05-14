3e7fa87f3f6f02c4dad5e5464d5aeb4f4c96c5e6
atsawa chakhonmettakul <atsawa.ch@inet.co.th>
2025-05-14 11:42:31 +0700

    edit base project

--- src/index.routes.ts
+++ src/index.routes.ts
@@ -1,8 +1,7 @@
 import Elysia from 'elysia';
 
 import { userRoute } from './routes/user.routes';
-import { authRoute } from './routes/auth.routes';
 
-const routes = new Elysia({ prefix: 'api/v1' }).use(userRoute).use(authRoute);
+const routes = new Elysia({ prefix: 'api/v1' }).use(userRoute);
 
 export { routes as AppRoutes };

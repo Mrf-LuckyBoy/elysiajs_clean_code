aea68f71e1176c6beac5b966c5521759756c644c
atsawa chakhonmettakul <atsawa.ch@inet.co.th>
2025-05-14 10:32:50 +0700

    add table migrations config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,10 @@ import { ENV } from '@/config/env';
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle/migrations',
-  driver: 'mysql2',
+  dialect: 'mysql',
   dbCredentials: {
     host: ENV.DB_HOST,
-    port: Number(ENV.DB_PORT || 3306),
+    port: Number(ENV.DB_PORT),
     user: ENV.DB_USER,
     password: ENV.DB_PASS,
     database: ENV.DB_NAME,

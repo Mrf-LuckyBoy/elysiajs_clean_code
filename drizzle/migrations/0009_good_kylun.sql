9733ebad2afaf9d9baa0966e2d3cfd609309ee28
Chanisara Prommuang <chanisara.pr@inet.co.th>
2025-05-29 15:52:53 +0700

    add column in screenings

---
+++ drizzle/migrations/0009_good_kylun.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `screenings` ADD `assign_official_service_unit` varchar(36);--> statement-breakpoint
+ALTER TABLE `screenings` ADD `assign_id_vhv` varchar(36);--> statement-breakpoint
+ALTER TABLE `screenings` ADD `diagnosis_id` varchar(36);

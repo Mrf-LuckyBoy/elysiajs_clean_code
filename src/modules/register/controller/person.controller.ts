7c6841a87eebe5039f10f931406d7e60c93a06d9
atsawa chakhonmettakul <atsawa.ch@inet.co.th>
2025-06-04 13:35:35 +0700

    pause edit

--- src/modules/register/controller/person.controller.ts
+++ src/modules/register/controller/person.controller.ts
@@ -43,11 +43,44 @@ export const personController = {
           set.status = 400;
           return HttpResponse.badRequest('กรอกข้อมูลในหน้าแรกไม่ครบ');
         }
+        if (
+          body.address_cid.hno === '' ||
+          body.address_cid.street === '' ||
+          body.address_cid.moo === '' ||
+          body.address_cid.villcode === ''
+        ) {
+          set.status = 400;
+          return HttpResponse.badRequest('ไม่มีข้อมูลที่อยู่ตามบัตรประชาชน');
+        } else if (
+          body.type_card === false ||
+          body.address_current.hno === '' ||
+          body.address_current.street === '' ||
+          body.address_current.moo === '' ||
+          body.address_current.villcode === ''
+        ) {
+          set.status = 400;
+          return HttpResponse.badRequest(
+            'ที่อยู่ปัจจุบันไม่เป็นที่อยู่เดียวกับที่อยู่ตามบัตรประชาชนแต่ไม่กรอกข้อมูลที่อยูปัจจุบัน'
+          );
+        }
+        if (
+          body.guardian.relationships === '' ||
+          body.guardian.idcard === '' ||
+          body.guardian.title === '' ||
+          body.guardian.first_name === '' ||
+          body.guardian.last_name === '' ||
+          body.guardian.phone === ''
+        ) {
+          set.status = 400;
+          return HttpResponse.badRequest('กรุณากรอกข้อมูลผู้ปกครอง');
+        }
+
         const result: NewRegisterFormDTO = await RegisterPerson(body);
 
         set.status = 201;
         return HttpResponse.success(result);
       } catch (err: unknown) {
+        console.log(err);
         if (err instanceof Error) {
           set.status = 500;
           return HttpResponse.error('some error detail');
